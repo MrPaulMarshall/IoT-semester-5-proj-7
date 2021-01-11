6e8a7118c14f327cc2e94b224c9790918cf4542e
wyder110 <wyder1234@gmail.com>
2021-01-11 18:19:10 +0100

    configuration and task-chance

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import threading as th
 import time
 
+import random
 from conf_loader import load_configuration
 from configuration import Configuration
 from node_draw import draw_configuration
@@ -10,13 +11,17 @@ from node_draw import reset_colors
 
 
 def start_app():
-    configuration = load_configuration("conf2.json")
+    configuration = load_configuration("configurations/conf4-4-4.json")
     for _, device in configuration.devices.items():
         print(device,  " parent: [", device.masterDevice , "] children = " , device.childrenDevices, " neighbours: ", device.neighbourDevices)
 
     drafter = Drafter.get_instance(configuration)
 
     cameras = configuration.cameras
+    # for _, device in configuration.devices.items():
+    #     print(device.maxComputingPower)
+    #     if device.maxComputingPower == 1:
+    #         cameras.append(device)
 
     print("--------SIMULATION--------")
 
@@ -25,12 +30,18 @@ def start_app():
         reset_colors(drafter.canvas, configuration)
         for _, device in configuration.devices.items():
             device.compute()
-        if i % 10 == 0:
-            for camera in cameras:
+
+        for camera in cameras:
+            if random.random() <= configuration.taskChance:
                 camera.sendFromCamera(camera.createTask(i), camera.masterDevice)
-        time.sleep(0.25)
+        time.sleep(1)
         drafter.root.update()
 
     drafter.root.mainloop()
+
+
+
+
+
 
 start_app()
